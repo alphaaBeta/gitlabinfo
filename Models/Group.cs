19c1d8623859fbb8b69b22efa721460473fcc7ae
Piotr Ogrodnik <piotr.ogrodnik@predica.pl>
2019-03-04 12:27:50 +0100

    Adds basic models and tests

---
+++ Models/Group.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GitlabInfo.Models
+{
+    public class Group
+    {
+        public Group(int gitLabId)
+        {
+            GitLabId = gitLabId;
+        }
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public int GitLabId { get; set; }
+        public ICollection<Project> Projects { get; set; }
+        public ICollection<UserGroup> AssignedUsers { get; set; }
+    }
+}
